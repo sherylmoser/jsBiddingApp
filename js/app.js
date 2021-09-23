81848c9405e24352b8a97175a66c74be5d31f3d4
sherylmoser <sherylrmoser@gmail.com>
2021-09-23 10:15:55 -0600

    some button styles, a few variables declared

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const bidElement = document.getElementById('bidArray');
+const input1 = document.getElementById('bid1').value;
+const input2 = document.getElementById('bid2').value;
+const bidArray = [];
+
+function displayBid() {
+    bidElement.innerText = bidArray
+}
